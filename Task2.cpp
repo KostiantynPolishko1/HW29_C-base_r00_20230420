b9ff72d1aebefaa38dff798a5544812eb57fa80d
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-04-27 15:47:11 +0300

    Task2 / 1 Start point

---
+++ Task2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include<fstream>
+#include<stdio.h>
+#include<Windows.h>
+
+using namespace std;
+
+//Task2
+//Write a transliteration program from Russian into English
+//and vice versa.The data for transliteration is taken from 
+//a file and written to another file. The translation direction 
+//is determined by the user menu.
+
+int main()
+{
+
+	return 0;
+}
