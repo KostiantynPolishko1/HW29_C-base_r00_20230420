390fe3e09bd8305f1636faad888007add311c1ad
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-04-27 16:00:17 +0300

    Task 2 2 Added function "translitRU_EN". The logic to substitute ru -> en letters

--- Task2.cpp
+++ Task2.cpp
@@ -12,8 +12,71 @@ using namespace std;
 //a file and written to another file. The translation direction 
 //is determined by the user menu.
 
+void translitRU_EN()
+{
+	setlocale(LC_ALL, "ru");
+
+	string
+		ru = "абвгдезийклмнопрстуфыэАБВГДЕЗИЙКЛМНОПРСТУФЫЭ",
+		ru2 = "жхцчшюяЖХЦЧШЮЯ",
+		ru3 = "щЩ";
+	string
+		en = "abvgdeziyklmnoprstufyeABVGDEZIYKLMNOPRSTUFYE",
+		en2 = "zhkhtschshyuyaZhKhTsChShYuYa",
+		en3 = "shchShch";
+	string en_txt{};
+
+	const char* str_ru{};
+	str_ru = "Полишко, ваше: имя и e-mail, URL адрес?";
+
+	cout << str_ru << endl;
+
+	while (*str_ru)
+	{
+		int pos = 0;
+		string s = {};
+		if (ru.find(*str_ru) != -1)
+		{
+			pos = ru.find(*str_ru);
+			s = en[pos];
+		}
+		else
+			if (ru2.find(*str_ru) != -1)
+			{
+				int i = 0;
+				pos = ru2.find(*str_ru);
+				i = pos * 2;
+				s += en2[i];
+				i++;
+				s += en2[i];
+			}
+			else
+				if (ru3.find(*str_ru) != -1)
+				{
+					int j = 0, count = 0;
+					pos = ru3.find(*str_ru);
+					j = pos * 4;
+					while (count < 4)
+					{
+						s += en3[j];
+						j++;
+						count++;
+					}
+				}
+				else
+				{
+					s = *str_ru;
+				}
+		str_ru++;
+		en_txt += s;
+	}
+
+	cout << en_txt << endl;
+}
+
 int main()
 {
+	translitRU_EN();
 
 	return 0;
 }
